c64cb2367dde4e6aa72ecc0e03ed5eba9e07dede
RitmicOfficial <137647756+RitmicOfficial@users.noreply.github.com>
2025-08-15 17:25:00 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,20 @@
+# Simple Calculator
+print("Addition: +")
+print("Subtraction: -")
+print("Multiplication: *")
+print("Division: /")
+
+num1 = float(input("Enter the first number: "))
+operation = input("Choose an operation (+, -, *, /): ")
+num2 = float(input("Enter the second number: "))
+
+if operation == "+":
+    print("Result:", num1 + num2)
+elif operation == "-":
+    print("Result:", num1 - num2)
+elif operation == "*":
+    print("Result:", num1 * num2)
+elif operation == "/":
+    print("Result:", num1 / num2)
+else:
+    print("Invalid operation")
